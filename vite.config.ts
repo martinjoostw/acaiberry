0474c43ccee3b06ec7df6db1f3fd0bb2393fbe51
Martin <martinbarriaweb@gmail.com>
2025-12-05 15:19:44 -0300

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/acaiberry/', // 👈 nombre del repo en GitHub
+  build: {
+    outDir: 'docs',    // 👈 Vite genera el build en /docs
+  },
 })
